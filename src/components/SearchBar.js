260f8fab7941bdeab021c2a11e50ca93d02db6b8
Sofiapaavola <paavola.sofia@gmail.com>
2022-01-20 11:50:32 +0000

    Added api call for cocktail database get request

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,10 +2,17 @@ import React from 'react'
 
 export default function SearchBar(props) {
     return (
-    <div>
-        <input type="text" 
+    <form onSubmit={props.getCocktails}> 
+        <input type='text'
+        name="nameOfCocktail"
         placeholder={props.placeholder}/>
-        <button>search</button>
-    </div>
+        <button style={buttonStyle} size='sm'>search</button>
+    </form>
     )
+}
+
+const buttonStyle = { 
+    backgroundColor: 'pink',
+    marginLeft: '2px',
+    border: 'pink'
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { Component }  from 'react'
 import SearchBar from '../components/SearchBar'
 
-export default function Home() {
-    return (
-    <div>
-        <SearchBar placeholder="search for a cocktail..."/>
-    </div>
-    )
+class Home extends Component { 
+    
+    getCocktails = async (e) => { 
+        const nameOfCocktail = e.target.elements.nameOfCocktail.value;
+        e.preventDefault();
+        const apiCall = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${nameOfCocktail}`);      
+        const result = await apiCall.json();
+        console.log(result)  
+  }
+
+    render() {
+        return (
+        <div>
+            <SearchBar getCocktails={this.getCocktails} placeholder="search for a cocktail..."/>
+        </div>
+        )
+    }
 }
+
+export default Home;
