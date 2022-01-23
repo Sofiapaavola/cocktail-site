f48571449f26db915df1a5cec082a9bb627dc533
Sofiapaavola <paavola.sofia@gmail.com>
2022-01-23 20:20:54 +0000

    Further styled the cards to look more clean

--- src/components/CocktailList/CocktailList.js
+++ src/components/CocktailList/CocktailList.js
@@ -8,15 +8,15 @@ export default function CocktailList(props) {
         <div className='row' style={{overflowX:'auto', flexWrap: 'nowrap'}}>
             {props.results.map((cocktail) => {
                 return (
-                <div className='col-4 card' style={{backgroundColor: '#D76735', width: '300px', padding: '10px', margin: '10px'}} key={cocktail.idDrink}>
-                    <img  className='card-img-top' src={cocktail.strDrinkThumb} alt={cocktail.strDrink}/>
-                    <div className='card-body'>
-                        <p className='card-text'> {cocktail.strDrink}</p>
-                        <button className='btn-sm' style={buttonStyle}>
+                <div className='card hover-styles' style={{backgroundColor: '#D76735', width: '500px', padding: '10px', margin: '10px'}} key={cocktail.idDrink}>
+                    <img  className='card-img' src={cocktail.strDrinkThumb} alt={cocktail.strDrink}/>
+                    <div className='card-img-overlay text-white d-flex flex-column justify-content-center'>
+                        <h1 className='card-title'> {cocktail.strDrink}</h1>
+                        <button className='btn' style={buttonStyle}>
                             <Link to={ `/instructions/${cocktail.idDrink}`} style={buttonStyle}>View Instructions</Link>
-                        </button>
-                        <button className='btn-sm' style={heartButtonStyle} onClick={ () => props.handleFavouritesClick(cocktail)}>
-                        <FavouriteComponent /> 
+                        </button> 
+                        <button className='btn' style={heartButtonStyle} onClick={() => props.handleFavouritesClick(cocktail)}>
+                            <FavouriteComponent /> 
                         </button>
                     </div>
                 </div> 
@@ -27,17 +27,14 @@ export default function CocktailList(props) {
 }
 
 const heartButtonStyle = {
-    color: '#643754',
-    fontSize: '100%',
+    color: 'white',
+    fontSize: '30px',
     border: '0', 
     padding: '0', 
-    backgroundColor: '#D76735'
 }
 
 const buttonStyle = { 
-    backgroundColor: '#EFD78D',
-    margin: '5px',
     border: '#D76735',
-    color: 'black',
+    color: 'white',
     textDecoration: 'none'
 }
