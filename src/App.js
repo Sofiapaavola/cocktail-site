16a84950af13e2479dd0864cbf970df808a9dce4
Sofiapaavola <paavola.sofia@gmail.com>
2022-01-24 13:40:32 +0000

    Third attempt to fix gh pages relative path issue

--- src/App.js
+++ src/App.js
@@ -6,14 +6,18 @@ import Support from './pages/Support';
 import Instructions from './pages/Instructions';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { createBrowserHistory } from "history";
 
 function App() {
+
+  const history = createBrowserHistory();
+
   return (
-      <Router>
+      <Router history={history}>
       <div className="App">
         <Navbar/>
         <div className="content">
-          <Routes basename="/cocktail-site">
+          <Routes>
             <Route path="/" element={<Home/>}/>
             <Route path="/about" element={<About/>}/>
             <Route path="/support" element={<Support/>}/>
