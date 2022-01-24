6b261a4079f53394f290e7b6392474c34e684a48
Sofiapaavola <paavola.sofia@gmail.com>
2022-01-24 00:25:57 +0000

    Added cocktail data to cocktailInstructionsComponent test to make it pass

--- src/components/CocktailInstructions/CocktailInstructionsComponent.js
+++ src/components/CocktailInstructions/CocktailInstructionsComponent.js
@@ -1,38 +1,40 @@
 import React from 'react';
 
-export default function CocktailInstructionsComponent(props) {
+export default function CocktailInstructionsComponent({
+    result
+}) {
     
     function isEmpty(value){
         if (value !== null) {
         return (
             <li className='list-group-item'>{value}</li> 
         ) }
-        else return( <div></div>);
+        else return(<div></div>);
     }
 
     return (
         <div className='row' style={{margin: '10px'}}> 
             <div className='col card' style={{backgroundColor: '#f2d8b3', borderColor: '#D76735', padding: '10px'}}>
-                <h1>{props.result.strDrink} Instructions</h1>
-                <img className='img-fluid' src={props.result.strDrinkThumb} alt={props.result.strDrink}/>
+                <h1 data-testid="mainInstructions">{result.strDrink} Instructions</h1>
+                <img className='img-fluid' src={result.strDrinkThumb} alt={result.strDrink}/>
             </div>
             <div className='col card d-flex' style={{backgroundColor: '#f2d8b3', borderColor: '#D76735',  marginLeft: '5px', padding: '10px'}}>
                 <div className='text-start'>
                     <h2 data-testid="ingredientsTitle">Ingredients</h2>
                     <ul className='list-group list-group-flush'>
-                        {isEmpty(props.result.strIngredient1)}
-                        {isEmpty(props.result.strIngredient2)}
-                        {isEmpty(props.result.strIngredient3)} 
-                        {isEmpty(props.result.strIngredient4)}
-                        {isEmpty(props.result.strIngredient5)}
-                        {isEmpty(props.result.strIngredient6)}
-                        {isEmpty(props.result.strIngredient7)}
-                        {isEmpty(props.result.strIngredient8)}
+                        {isEmpty(result.strIngredient1)}
+                        {isEmpty(result.strIngredient2)}
+                        {isEmpty(result.strIngredient3)} 
+                        {isEmpty(result.strIngredient4)}
+                        {isEmpty(result.strIngredient5)}
+                        {isEmpty(result.strIngredient6)}
+                        {isEmpty(result.strIngredient7)}
+                        {isEmpty(result.strIngredient8)}
                     </ul>   
                 </div>
                 <div className='text-start' style={{marginTop: '10px'}}>
                     <h2 data-testid="instructionsCardTitle">Instructions</h2>
-                    <p style={{backgroundColor: 'white'}}>{props.result.strInstructions}</p>
+                    <p style={{backgroundColor: 'white'}}>{result.strInstructions}</p>
                 </div>
             </div>
         </div>
--- src/components/CocktailInstructions/CocktailInstructionsList.test.js
+++ src/components/CocktailInstructions/CocktailInstructionsList.test.js
@@ -2,10 +2,16 @@ import { render, screen } from '@testing-library/react';
 import CocktailInstructionsComponent from './CocktailInstructionsComponent';
 import '@testing-library/jest-dom/extend-expect';
 
-test('renders correct titles for the instructions page', () => {
-  const {getByTestId} = render(<CocktailInstructionsComponent />);
-  const title = getByTestId("ingredientsTitle"); 
-  expect(title.textContent).toBe("Ingredients")
+const cocktail = { "idDrink":"17216","strDrink":"Tommy's Margarita","strDrinkAlternate":null,"strTags":"IBA,NewEra","strVideo":null,"strCategory":"Ordinary Drink","strIBA":"New Era Drinks","strAlcoholic":"Alcoholic","strGlass":"Old-Fashioned glass","strInstructions":"Shake and strain into a chilled cocktail glass.","strInstructionsES":null,"strInstructionsDE":"Sch\u00fctteln und in ein gek\u00fchltes Cocktailglas abseihen.","strInstructionsFR":null,"strInstructionsIT":"Shakerare e filtrare in una coppetta da cocktail ghiacciata.","strInstructionsZH-HANS":null,"strInstructionsZH-HANT":null,"strDrinkThumb":"https:\/\/www.thecocktaildb.com\/images\/media\/drink\/loezxn1504373874.jpg","strIngredient1":"Tequila","strIngredient2":"Lime Juice","strIngredient3":"Agave syrup","strIngredient4":null,"strIngredient5":null,"strIngredient6":null,"strIngredient7":null,"strIngredient8":null,"strIngredient9":null,"strIngredient10":null,"strIngredient11":null,"strIngredient12":null,"strIngredient13":null,"strIngredient14":null,"strIngredient15":null,"strMeasure1":"4.5 cl","strMeasure2":"1.5 cl","strMeasure3":"2 spoons","strMeasure4":null,"strMeasure5":null,"strMeasure6":null,"strMeasure7":null,"strMeasure8":null,"strMeasure9":null,"strMeasure10":null,"strMeasure11":null,"strMeasure12":null,"strMeasure13":null,"strMeasure14":null,"strMeasure15":null,"strImageSource":null,"strImageAttribution":null,"strCreativeCommonsConfirmed":"No","dateModified":"2017-09-02 18:37:54"}
+
+test('renders correct titles for the instructions page', async () => {
+  const {getByTestId} = render(<CocktailInstructionsComponent result={cocktail} />);
+  
+  const title = getByTestId("mainInstructions"); 
+  expect(title.textContent).toBe("Tommy's Margarita Instructions")
+ 
+  const title1 = getByTestId("ingredientsTitle"); 
+  expect(title1.textContent).toBe("Ingredients")
 
   const title2 = getByTestId("instructionsCardTitle"); 
   expect(title2.textContent).toBe("Instructions")
