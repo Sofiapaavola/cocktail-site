75953b7ec3bc61a770280fe460d9fb00cc868d4b
Sofiapaavola <paavola.sofia@gmail.com>
2022-03-23 23:30:22 +0000

    refactored Home.js to be functional component rather than class based

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,86 +1,86 @@
-import React, { Component }  from 'react';
+import React, { Component, useState, useEffect}  from 'react';
 import SearchBar from '../components/SearchBar/SearchBar';
 import CocktailList from '../components/CocktailList/CocktailList';
 import Heading from '../components/Heading/Heading';
 import AddFavourite from '../components/Favourites/AddFavourite';
 import RemoveFavourite from '../components/Favourites/RemoveFavourite';
 
-export default class Home extends Component { 
+export default function Home() { 
 
-    state = {
-        cocktailResultsObject: [],
-        favourites: [],
-        cache: [],
-        cacheTimer: 0,
-        cacheTime: 10000, 
-        isLoading: false
-    } 
+    const [cocktailResultsObject, setObject] = useState([]);
+    const [favourites, setFavourites] = useState([]);
+    const [cache, setCache] = useState([]);
+    const [cacheTimer, setCacheTimer] = useState(0);
+    const [cacheTime, setCacheTime] = useState(1000);
+    const [isLoading, setLoading] = useState(false);
 
-    componentDidMount() {
-        const cocktailFavourites = JSON.parse(
-            localStorage.getItem('react-cocktail-app-favourites')
-        );
-        if (cocktailFavourites) {
-            this.setState({favourites: cocktailFavourites})
-		}
+    useEffect(() => {
+    const cocktailFavourites = JSON.parse(
+        localStorage.getItem('react-cocktail-app-favourites')
+    );
+    if (cocktailFavourites) {
+        setFavourites(cocktailFavourites)
     }
+    }, []);
 
-    getCacheTimer = time => {
+    
+    const getCacheTimer = time => {
         const now = new Date().getTime();
-        if (this.state.cacheTimer < now + time) {
-            this.state.cacheTimer = now + time
+        if (cacheTimer < now + time) {
+            cacheTimer = now + time
         }
-        return this.state.cacheTimer;
+        return cacheTimer;
     }
 
-    fetchWithCache = async (e, time) => { 
+    const fetchWithCache = async (e, time) => { 
         const cocktailName = e.target.elements.nameOfCocktail.value;
         const now = new Date().getTime();
-        if (!this.state.cache[cocktailName] || this.state.cache[cocktailName].cacheTimer < now) { 
-            this.setState({cache: await this.getCocktails(cocktailName)})
-            this.setState({cacheTimer: this.getCacheTimer(time)})
-            //this.state.cache[cocktailName].cacheTimer = this.getCacheTimer(time)
+        if (!cache[cocktailName] || cache[cocktailName].cacheTimer < now) { 
+            setCache(await getCocktails(cocktailName))
+            setCacheTimer(getCacheTimer(time))
+            //cache[cocktailName].cacheTimer = getCacheTimer(time)
         }
-        console.log('cache', this.state.cache)
-        return this.state.cache
+        console.log('cache', cache)
+        return cache
     }
 
-    getCocktails = async (e) => { 
+    const getCocktails = async (e) => { 
         const cocktailName = e.target.elements.nameOfCocktail.value;
         e.preventDefault();
-        this.setState({isLoading: true})
+        setLoading(true);
         const apiCall = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktailName}`);      
         const result = await apiCall.json();
-        this.setState({cocktailResultsObject: result.drinks, isLoading: false})
+        setObject(result.drinks);
+        setLoading(false);
     }
 
-    saveToLocalStorage = (items) => {
+    const saveToLocalStorage = (items) => {
         localStorage.setItem('react-cocktail-app-favourites', JSON.stringify(items))
     }
 
-    addFavouriteCocktail = (cocktail) => { 
-        const newFavouriteCocktails = [...this.state.favourites, cocktail]
-        this.setState({favourites: newFavouriteCocktails});
-        this.saveToLocalStorage(newFavouriteCocktails);
+    const addFavouriteCocktail = (cocktail) => { 
+        const newFavouriteCocktails = [...favourites, cocktail]
+        setFavourites(newFavouriteCocktails)
+        saveToLocalStorage(newFavouriteCocktails);
     }
 
-    removeFavouriteCocktail = (cocktail) => { 
-        const newFavouriteCocktails = this.state.favourites.filter( 
+    const removeFavouriteCocktail = (cocktail) => { 
+        const newFavouriteCocktails = favourites.filter( 
             (favourite) => favourite.idDrink !== cocktail.idDrink
         );
-        this.setState({favourites: newFavouriteCocktails});
-        this.saveToLocalStorage(newFavouriteCocktails);
+        setFavourites(newFavouriteCocktails)
+        saveToLocalStorage(newFavouriteCocktails);
     }
 
-    renderFavourites() { 
-        if (this.state.favourites !== null && this.state.favourites.length > 0) { 
+    const renderFavourites = () => { 
+        if (favourites !== null && favourites.length > 0) { 
             return ( 
             <>
                 <Heading title="Faves"></Heading>  
                 <div className='row'>
                     <CocktailList 
-                    results={this.state.favourites}
-                    handleFavouritesClick={this.removeFavouriteCocktail} 
+                    results={favourites}
+                    handleFavouritesClick={removeFavouriteCocktail} 
                     favouriteComponent={RemoveFavourite}
                     />
                 </div>
@@ -89,21 +89,20 @@ export default class Home extends Component {
         } else return <></>           
     }
 
-    render() {
-        return (
-        <div>
-            <SearchBar getCocktails={this.getCocktails} placeholder="search for a cocktail..."/>
-            {this.state.isLoading && <div> Loading...</div>} 
-            <div style={{padding: '10px'}}>     
-                <div className='row'>
-                    <CocktailList 
-                    handleFavouritesClick={this.addFavouriteCocktail} 
-                    favouriteComponent={AddFavourite}
-                    results={this.state.cocktailResultsObject}/>
-                </div>
-                {this.renderFavourites()}
+    return (
+    <div>
+        <SearchBar getCocktails={getCocktails} placeholder="search for a cocktail..."/>
+        {isLoading && <div> Loading...</div>} 
+        <div style={{padding: '10px'}}>     
+            <div className='row'>
+                <CocktailList 
+                handleFavouritesClick={addFavouriteCocktail} 
+                favouriteComponent={AddFavourite}
+                results={cocktailResultsObject}/>
             </div>
+            {renderFavourites()}
         </div>
-        )
-    }
+    </div>
+    )
+
 }
